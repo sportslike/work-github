d921e6b9b50171ea43d326f89d28c15dfc6a302f
Shunsuke135 <shun.135.2109@gmail.com>
2021-01-21 12:45:47 +0000

    新規顧客登録画面etc...

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  
-  devise_for :users, controllers: {
-    sessions:      'users/sessions',
-    passwords:     'users/passwords',
-    registrations: 'users/registrations'
-  }
 
-   devise_for :admins, controllers: {
+
+  namespace :public do
+    get 'homes/top'
+  end
+  devise_for :customers, controllers: {
+     sessions:      'devise/publics/sessions',
+     passwords:     'devise/publics/passwords',
+     registrations: 'devise/publics/registrations'
+   }
+
+
+  devise_for :admins, controllers: {
      sessions:      'devise/admins/sessions',
      passwords:     'devise/admins/passwords',
      registrations: 'devise/admins/registrations'
