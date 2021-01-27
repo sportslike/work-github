15fc78c2a22abce642a0135c5e03e6c701211eec
Shunsuke135 <shun.135.2109@gmail.com>
2021-01-27 08:30:34 +0000

    マイページのメールアドレス編集○

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -33,7 +33,7 @@ class Public::CustomersController < ApplicationController
   private
 
   def customer_params
-    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :telephone_number, :postal_code, :address)
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :telephone_number, :postal_code, :address, :email)
   end
 
 end
