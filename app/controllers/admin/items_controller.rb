97e236669636efe4e31afea431a1c09f398e8eb0
machida15 <shun8568@gmail.com>
2021-01-27 07:06:47 +0000

    path修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,7 +11,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to admin_items_path, notice: "商品を登録しました"
+      redirect_to admin_item_path(@item.id), notice: "商品を登録しました"
     else
       render :new
     end
